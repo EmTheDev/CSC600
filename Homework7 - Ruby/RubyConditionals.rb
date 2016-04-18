5593b4837f9c797b01839cfb99f92d4bbc35f9d8
EmTheDev <EmTheDev@gmail.com>
2016-04-17 23:08:28 -0700

    Started on dumb ruby stuff

---
+++ Homework7 - Ruby/RubyConditionals.rb
@@ -0,0 +1,22 @@
+=begin
+1. Write a Ruby demo program that illustrates the use of all 
+main Ruby conditional statements.
+=end
+
+x = 5
+y = 5
+
+#if-then
+if x = y 
+	then puts "x and y are equal" 
+end
+
+
+#if-else
+if x+y = 10
+	puts "The sum of x and y is 10"
+else
+	puts "The sum of x and y is not 10"
+end
+
+#if-elsif-else
