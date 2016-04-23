bdb27f285456f55c5d042aa6485d8c35c27783ae
EmTheDev <EmTheDev@gmail.com>
2016-04-23 13:47:49 -0700

    Finished part one

--- Homework7 - Ruby/RubyConditionals.rb
+++ Homework7 - Ruby/RubyConditionals.rb
@@ -13,10 +13,40 @@ end
 
 
 #if-else
-if x+y = 10
+if (x+y == 10)
 	puts "The sum of x and y is 10"
 else
 	puts "The sum of x and y is not 10"
 end
 
-#if-elsif-else
+#if-elsif
+z = 11
+if z > y
+	puts "z is greater than the sum of x and y"
+elsif z < x
+	puts "z is less than the sum of x and y"
+else
+	puts "z is equal to the sum of x and y"
+end
+
+#unless
+unless x+y>z
+	puts "z is greater than or equal to the sum of x and y"
+else
+	puts "z is less than the sum of x and y"
+end
+
+#modifiers
+puts "z > x+y" if z > x+y
+puts "x+y > z" unless z >x+y
+
+#case
+finalGrade = 72
+letterGrade = case finalGrade
+	when 0..59 then "F"
+	when 60..69 then "D"
+	when 70..79 then "C"
+	when 80..89 then "B"
+	when 90..100 then "A"
+	end
+puts "Final Grade: " + letterGrade
