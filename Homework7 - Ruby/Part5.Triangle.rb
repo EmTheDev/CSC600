1a63de056e9e9fa196f5b74c908480a27d46d26d
EmTheDev <EmTheDev@gmail.com>
2016-04-24 12:59:59 -0700

    testing format

--- Homework7 - Ruby/Part5.Triangle.rb
+++ Homework7 - Ruby/Part5.Triangle.rb
@@ -1,3 +1,9 @@
 =begin
-5. Create a Ruby class triangle with initalizer, accessors, and member functions for computing the perimeter and the area of arbitrary triangles. Make also a member function test that checks sides a, b, and c and classifies the triangle as (1) equilateral, (2) isosceles, (3) scalene, (4) right, and (5) not a triangle. Right triangle can be either isosceles or scalene. Compute the perimeter and area only for valid triangles (verified by test). Show examples of the use of this class.
+5. Create a Ruby class triangle with initalizer, accessors, and member 
+functions for computing the perimeter and the area of arbitrary triangles. 
+Make also a member function test that checks sides a, b, and c and classifies 
+the triangle as (1) equilateral, (2) isosceles, (3) scalene, (4) right, 
+and (5) not a triangle. Right triangle can be either isosceles or scalene. 
+Compute the perimeter and area only for valid triangles (verified by test). 
+Show examples of the use of this class.
 =end
